84f0fad57eb6a955f94e54d45fc1c6839b44846a
Irma Blanco <irblancofdz@gmail.com>
2020-10-19 21:06:02 +0200

    feat: Api scaffolding

---
+++ models/place.model.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+
+const placeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Place title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    url: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: [true, "Image is required"],
+    },
+    // TO DO: Vamos a incluir creator en el place????
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    openingHours: {
+      type: [String],
+    },
+
+    city: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    location: {
+      type: { type: String },
+      coordinates: [Number],
+    },
+    recommended: {
+      type: Boolean,
+      default: false,
+    },
+    googleId: {
+      type: String,
+    },
+    tags: [String],
+    rating: Number,
+    priceLevel: Number,
+  },
+  { timestamps: true }
+);
+
+const Place = mongoose.model("Place", placeSchema);
---
+++ models/tour.model.js
@@ -0,0 +1,64 @@
+const Place = require("./place.model");
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Tour title is required"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Tour description is required"],
+      trim: true,
+    },
+    city: {
+      type: String,
+      trim: true,
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    image: {
+      type: String,
+      default: "/img/tour-def-icon.svg",
+    },
+    recommended: {
+      type: Boolean,
+      default: false,
+    },
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    // TO DO: Comprobacion de que el placeId debe ser unique
+    places: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Place",
+        },
+      ],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+  { timestamps: true }
+);
+
+tourSchema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "tour",
+  justOne: false,
+});
+
+tourSchema.post("remove", function (next) {
+  Promise.all([Comment.deleteMany({ place: this._id })]).then(next);
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
