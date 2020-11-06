46801cb109994ee4a20165f8523d762604829866
Irma Blanco <irblancofdz@gmail.com>
2020-11-06 23:32:39 +0100

    allow headers

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ const cors = require("./config/cors.config");
  * Configure express
  */
 const app = express();
-app.use(cors);
+// app.use(cors);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
