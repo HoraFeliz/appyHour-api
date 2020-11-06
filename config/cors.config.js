665ba265e5877c8ed62f1dea2162d556ae0da96c
Irma Blanco <irblancofdz@gmail.com>
2020-11-06 23:24:26 +0100

    CORS

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,7 +1,8 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN_HEADER || "https://appy-hour.netlify.app",
+  origin:
+    process.env.CORS_ORIGIN_HEADER || "https://appy-hour-react.netlify.app",
   allowedHeaders: ["Content-Type"],
   credentials: true,
 });
