c5d7de7393a1d4c692bd0b0445e78278bfea9136
Irma Blanco <irblancofdz@gmail.com>
2020-12-11 16:40:34 +0100

    more cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -2,7 +2,12 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
   origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-  allowedHeaders: ["Content-Type", "X-Auth-Token"],
+  allowedHeaders: [
+    "Content-Type",
+    "X-Auth-Token",
+    "Access-Control-Allow-Headers",
+    "Authorization",
+  ],
   credentials: true,
 });
 
