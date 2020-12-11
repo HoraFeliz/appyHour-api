7ef4de5510bf4e62d0300459ca89b6f1690160b1
Irma Blanco <irblancofdz@gmail.com>
2020-12-11 16:46:20 +0100

    revert: Cors config

--- config/cors.config.js
+++ config/cors.config.js
@@ -2,13 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
   origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-  allowedHeaders: [
-    "Access-Control-Allow-Headers",
-    "Content-Type",
-    "X-Auth-Token",
-    "Authorization",
-    "Access-Control-Allow-Origin",
-  ],
+  allowedHeaders: ["Content-Type"],
   credentials: true,
 });
 
