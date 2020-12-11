b2e01d33a405509501329f32a3d3c2c59675364f
Irma Blanco <irblancofdz@gmail.com>
2020-12-11 16:41:23 +0100

    Access-Control-Allow-Origin

--- config/cors.config.js
+++ config/cors.config.js
@@ -3,10 +3,11 @@ const cors = require("cors");
 const corsMiddleware = cors({
   origin: process.env.CORS_ORIGIN || "http://localhost:3000",
   allowedHeaders: [
+    "Access-Control-Allow-Headers",
     "Content-Type",
     "X-Auth-Token",
-    "Access-Control-Allow-Headers",
     "Authorization",
+    "Access-Control-Allow-Origin",
   ],
   credentials: true,
 });
