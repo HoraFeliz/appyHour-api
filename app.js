4df288809d46a7f95866460c8ef9dc52dd0872ab
Irma Blanco <irblancofdz@gmail.com>
2020-12-11 16:37:38 +0100

    test: Cors

--- app.js
+++ app.js
@@ -10,30 +10,13 @@ const mongoose = require("mongoose");
 require("./config/db.config");
 const passportConfig = require("./config/passport.config");
 const session = require("./config/session.config");
-// const cors = require("./config/cors.config");
+const cors = require("./config/cors.config");
 
 /**
  * Configure express
  */
 const app = express();
-// app.use(cors);
-const allowedOrigin = process.env.CORS_ORIGIN || "http://localhost:3000";
-app.use((req, res, next) => {
-  res.header(`Access-Control-Allow-Origin: ${allowedOrigin}`);
-  res.header(
-    "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token"
-  );
-  res.header(
-    "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token"
-  );
-  next();
-});
+app.use(cors);
 
 app.use(logger("dev"));
 app.use(express.json());
