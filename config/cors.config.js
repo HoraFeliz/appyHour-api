66c4cc2060c0e33ae8a12066a4e19bc3cc04432f
aitorgb <aitorguerrero9@gmail.com>
2021-12-10 17:39:36 +0100

    test cors

--- config/cors.config.js
+++ config/cors.config.js
@@ -1,9 +1,10 @@
 const cors = require("cors");
 
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:3000",
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: "*",
+ // allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 });
 
 module.exports = corsMiddleware;
+// origin: process.env.CORS_ORIGIN || "http://localhost:3000",
